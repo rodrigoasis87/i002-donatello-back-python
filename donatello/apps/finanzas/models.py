96bcd7889aaa736193b8db03c5ffa9a98b64b5a3
Rodrigo <rodrigoasis87@gmail.com>
2024-07-11 14:23:12 -0300

    últimos cambios

--- donatello/apps/finanzas/models.py
+++ donatello/apps/finanzas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Transaccion(models.Model):
+    TIPO_CHOICES = [
+        ('income', 'Ingreso'),
+        ('spend', 'Gasto'),
+    ]
+    type = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.quantity} el {self.date}"
--- donatello/settings.py
+++ donatello/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.finanzas'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'donatello.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'donatello.wsgi.application'
 
 
 # Database
